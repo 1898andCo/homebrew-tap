f16116e5c1a1e238847f5eb370b8b1417e39035f
Joshua Magady <josh.magady@gmail.com>
2026-03-14 07:58:48 +0000

    tally 0.2.0

--- Formula/tally.rb
+++ Formula/tally.rb
@@ -1,8 +1,8 @@
 class Tally < Formula
   desc "Git-backed findings tracker for AI coding agents"
   homepage "https://github.com/1898andCo/tally"
-  url "https://github.com/1898andCo/tally/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1f24d21d55c83baf74fe79ce49eaec0d7fe5a8ea538ec90c597896dcc9957184"
+  url "https://github.com/1898andCo/tally/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "94498818175e155ea98c9e075be9ec8b1fce5f2039c58834ce7e49466fb7eb55"
   license "Apache-2.0"
   head "https://github.com/1898andCo/tally.git", branch: "develop"
 
